92f864e96ca6cff0e8ce89c03526fee20fb5d0b2
Marcus W <marcuswsywong@gmail.com>
2024-03-09 15:28:59 +0000

    Refactor and add ui
    Refactor the logic from input to buttons for user selection
    Add UI for buttons
    Add display for score live
    ADd display for winner when 5 points reached

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const playerScoreDisplay = document.getElementById("your-score")
+const userScoreDisplay = document.getElementById("your-score")
 const computerScoreDisplay = document.getElementById("computer-score")
+const resultDisplay = document.querySelector("#result-display")
+const winnerDisplay = document.querySelector("#winner-display")
+const rockBtn = document.querySelector("#rock-btn")
+const paperBtn = document.querySelector("#paper-btn")
+const scissorsBtn = document.querySelector("#scissors-btn")
 
 let userScore = 0;
 let computerScore = 0;
+let result = ""
 
 // Make an ai answer 
 const getComputerChoice = () => {
@@ -11,54 +17,52 @@ const getComputerChoice = () => {
     return generateComputerChoice
 }
 
-
-// get user answer
-const getUserChoice = () => {
-    let userChoiceRaw = window.prompt("Rock Paper Scissors Go!")
-    return userChoiceRaw.charAt(0).toUpperCase() + userChoiceRaw.slice(1).toLowerCase();
-}
-
-
-
 // Play single round, take computerSelection and playerSelection
 const calculateRound = (userChoice, computerChoice) => { 
     if (userChoice != "Rock" && userChoice != "Paper" && userChoice != "Scissors") {
-        console.log("Invalid") 
+        result = "Invalid";
     } else if (userChoice === computerChoice) {
-        console.log(computerChoice)
-        console.log("Draw")
+        result = "Draw";
     } else if (userChoice === "Rock" && computerChoice === "Scissors") {
-        console.log("R win S")
+        result = "R win S";
         userScore++
     } else if (userChoice === "Rock" && computerChoice === "Paper") {
-        console.log("R lose P")
+        result = "R lose P";
         computerScore++
     } else if (userChoice === "Paper" && computerChoice === "Rock") {
-        console.log("P win R")
+        result = "P win R";
         userScore++
     } else if (userChoice === "Paper" && computerChoice === "Scissors") {
-        console.log("P lose S")
+        result = "P lose S";
         computerScore++
     } else if (userChoice === "Scissors" && computerChoice === "Paper") {
-        console.log("S win P")
+        result = "S win P";
         userScore++
     } else if (userChoice === "Scissors" && computerChoice === "Rock") {
-        console.log("S lose R")
+        result = "S lose R";
         computerScore++
     }
 }
 
-console.log(calculateRound("Rock", "Rock"))
-
-
-const playRound = () => {
-    return calculateRound(getUserChoice(), getComputerChoice()) 
+const playRound = (userSelection) => {
+    calculateRound(userSelection, getComputerChoice()) 
+    userScoreDisplay.innerText = `Your Score: ${userScore}`
+    computerScoreDisplay.innerText =  `Computer Score: ${computerScore}`
+    resultDisplay.innerText = result
+    announceWinner()
 }
 
-while (userScore < 5 && computerScore < 5) {
-    playRound()
-    playerScoreDisplay.innerText = userScore
-    computerScoreDisplay.innerText = computerScore
+// get user answer to compare against ai answer
+rockBtn.addEventListener("click", () => playRound("Rock"))
+paperBtn.addEventListener("click", () => playRound("Paper"))
+scissorsBtn.addEventListener("click", () => playRound("Scissors"))
+
+const announceWinner = () => {
+    if (userScore >= 5) {
+        winnerDisplay.textContent = "You Win!"
+    } else if (computerScore >= 5) {
+        winnerDisplay.textContent = "You Lose!"
+    }
 }
 
 // calculate winner if you win your score ++
